0f7e5eb0a8db2d91224aa2c97d579e8915d12343
Junior Sfredo <gilvan.sfr@gmail.com>
2025-05-07 10:57:41 -0300

    Refactoring SplashPage and create SplashBottom

--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -1,5 +1,6 @@
 import 'package:course_plataform_ui/common/constants/color_const.dart';
 import 'package:course_plataform_ui/pages/splash/sections/splash_animate.dart';
+import 'package:course_plataform_ui/pages/splash/sections/splash_bottom.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:lottie/lottie.dart';
@@ -39,69 +40,7 @@ class _SplashPageState extends State<SplashPage> {
                 child: SizedBox(
                   height: height * 0.45,
                   width: double.infinity,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: ColorsConst.lightBackgroundGradient,
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                      ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 40, right: 40),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Seu futuro começa com uma nova habilidade!',
-                            style: GoogleFonts.figtree(
-                              height: 1.3,
-                              wordSpacing: 1 / 5,
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                              color: ColorsConst.darkBackgroundPrimary,
-                            ),
-                          ),
-                          SizedBox(height: 30),
-                          Text(
-                            'Aprenda novas habilidades com cursos práticos e interativos, '
-                            'em diversas áreas do conhecimento.',
-                            style: GoogleFonts.figtree(
-                              height: 1.4,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                              color: ColorsConst.darkBackgroundPrimary,
-                            ),
-                          ),
-                          const SizedBox(height: 60),
-                          Align(
-                            alignment: Alignment.bottomRight,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(100),
-                                border: Border.all(
-                                  color: ColorsConst.darkBackgroundPrimary,
-                                  width: 3,
-                                ),
-                              ),
-                              child: CircleAvatar(
-                                backgroundColor: Colors.transparent,
-                                radius: 30,
-                                child: FaIcon(
-                                  FontAwesomeIcons
-                                      .arrowRight, // Ícone do Font Awesome
-                                  size: 25, // Tamanho do ícone
-                                  color:
-                                      ColorsConst
-                                          .backgroundPrimary, // Cor do ícone
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                  child: SplashBottom(),
                 ),
               ),
             SplashAnimate(shrink: _shrink, onLoaded: _animateLoader),
